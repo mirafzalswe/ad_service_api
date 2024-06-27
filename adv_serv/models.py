1c8d15553c1ccdc26db23ad0d6dcb395ecb7950d
mirafzalswe <mirafzaldevelop@gmail.com>
2024-06-27 15:48:29 +0500

    Добавлены модели

--- adv_serv/models.py
+++ adv_serv/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Ad(models.Model):
+    title = models.CharField(max_length=255)
+    ad_id = models.IntegerField(unique=True)
+    author = models.CharField(max_length=255)
+    views = models.IntegerField()
+    position = models.IntegerField()
+
+    def __str__(self):
+        return self.title
